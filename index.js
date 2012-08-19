171643b0aa6cd0409425fcf39dfb21cf14630fd7
Brian White <mscdex@mscdex.net>
2012-08-19 15:43:19 -0400

    Initial js-land Client wrapper

--- index.js
+++
@@ -1,6 +0,0 @@
-var addon = require('./build/Release/sqlclient');
-var EventEmitter = require('events').EventEmitter;
-
-addon.Client.prototype.__proto__ = EventEmitter.prototype;
-
-module.exports = addon;
