9faf7d75981acb680b992d720d0fa766d9a582f2
Brian White <mscdex@mscdex.net>
2015-09-16 00:27:45 -0400

    deps: update libmariadbclient to 10.0.21

--- deps/libmariadbclient/include/mysql_version.h
+++ deps/libmariadbclient/include/mysql_version.h
@@ -1,5 +1,7 @@
-/* Copyright Abandoned 1996, 1999, 2001 MySQL AB
-   This file is public domain and comes with NO WARRANTY of any kind */
+/* Copyright Abandoned 1996,1999 TCX DataKonsult AB & Monty Program KB
+   & Detron HB, 1996, 1999-2004, 2007 MySQL AB.
+   This file is public domain and comes with NO WARRANTY of any kind
+*/
 
 /* Version numbers for protocol & mysqld */
 
@@ -8,24 +10,24 @@
 #ifdef _CUSTOMCONFIG_
 #include <custom_conf.h>
 #else
-#define PROTOCOL_VERSION		10
-#define MYSQL_SERVER_VERSION		"5.5.25-MariaDB"
-#define MYSQL_BASE_VERSION		"mysqld-5.5"
-#define MARIADB_BASE_VERSION		"mariadb-5.5"
-#define MYSQL_SERVER_SUFFIX_DEF		""
-#define FRM_VER				6
-#define MYSQL_VERSION_ID		50525
-#define MYSQL_PORT			3306
-#define MYSQL_PORT_DEFAULT		0
-#define MYSQL_UNIX_ADDR			"/tmp/mysql.sock"
-#define MYSQL_CONFIG_NAME		"my"
-#define MYSQL_COMPILATION_COMMENT	"Source distribution"
+#define PROTOCOL_VERSION                10
+#define MYSQL_SERVER_VERSION            "10.0.21-MariaDB"
+#define MYSQL_BASE_VERSION              "mysqld-10.0"
+#define MARIADB_BASE_VERSION            "mariadb-10.0"
+#define MYSQL_SERVER_SUFFIX_DEF         ""
+#define FRM_VER                         6
+#define MYSQL_VERSION_ID                100021
+#define MYSQL_PORT                      3306
+#define MYSQL_PORT_DEFAULT              0
+#define MYSQL_UNIX_ADDR                 "/tmp/mysql.sock"
+#define MYSQL_CONFIG_NAME               "my"
+#define MYSQL_COMPILATION_COMMENT       "MariaDB Server"
 
 /* mysqld compile time options */
 #endif /* _CUSTOMCONFIG_ */
 
 #ifndef LICENSE
-#define LICENSE				GPL
+#define LICENSE                         GPL
 #endif /* LICENSE */
 
 #endif /* _mysql_version_h */
